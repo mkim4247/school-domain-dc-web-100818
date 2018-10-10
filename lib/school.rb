78d6983f65e822ab3c69f630b43b5eac8f82cfc1
Michael Kim <m.kim4247@gmail.com>
2018-10-10 13:57:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student_name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student_name
+    else 
+      @roster[grade] << student_name
+    end 
+  end 
+  
+end 
