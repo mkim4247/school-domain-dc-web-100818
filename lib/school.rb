c154c0fcf95d40f4b863032558a882aa3e4ea37b
Michael Kim <m.kim4247@gmail.com>
2020-12-28 15:54:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,11 @@
 class School
-  attr_accessor :name, :roster
-  
   def initialize(name)
     @name = name
-    @roster = {}
-  end 
-
-  def add_student(student_name, grade)
-    if @roster[grade] ||= []
-      @roster[grade] << student_name
-    end 
-  end 
-    
-  #   if @roster.keys.include?(grade)
-  #     @roster[grade] << student_name
-  #   else  
-  #     @roster[grade] = []
-  #     @roster[grade] << student_name
-  #   end 
-  # end 
-    
-  def grade(grade)
-    @roster[grade]
-  end 
+  end
   
-  def sort
-    sorted_roster = {}
-    @roster.each do |grade, student_list|
-      sorted_roster[grade] = student_list.sort
-    end 
-    sorted_roster
-  end 
-end 
-
+  attr_reader :name
+  
+  ROSTER = {}
+  
+  
+end
